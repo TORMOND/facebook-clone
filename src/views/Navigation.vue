6e941ecd2381341ba9cfa9cf0ab29ae9aeceba46
tormond <victormonderu@gmail.com>
2021-11-17 15:58:29 -0800

    commit on master

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -12,11 +12,10 @@
            <span><i class="fab fa-youtube"></i></span>
              <span><i class="fas fa-store"></i></span>
              <!-- <span class="material-icons-outlined">storefront</span>  -->
-             <span class="user"><i class="fas fa-users"></i></span>
+             <span><label class="user"><i class="fas fa-users"></i></label></span>
           </div>
           <div class="extras">
               <span><i class="fas fa-user"></i></span>
-              
              <span><i class="fas fa-plus"></i></span>
               <span><i class="fab fa-facebook-messenger"></i></span>
               <span><i class="fas fa-bell"></i></span>
@@ -25,18 +24,21 @@
       </nav>
       <div class="container">
 <div class="grid">
-<div class="part-1"></div>
+<div class="part-1">
+    <!-- <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, possimus repudiandae veritatis, dolor quisquam corrupti vel optio, numquam porro dolorum eum doloremque provident autem ad dolore dolorem iste aperiam nihil.</p>
+    <input type="file" placeholder="add image"> -->
+</div>
 <div class="content">
 
 <div class="wrap">
     <div class="post">
   <span><i class="fas fa-user"></i></span>
-  <input type="text" placeholder="Whats on Your mind, Victor?">
+  <input type="text" placeholder="Whats on Your mind, Victor?" @click="open">
     </div>
-  <div>
-<label><i class="far fa-images"></i></label>
-<label><i class="fas fa-user-tag"></i></label>
-<label><i class="fas fa-laugh"></i></label>
+  <div class="post-2">
+<label  @click="open"><i class="far fa-images"  ></i>Photo/Video</label>
+<label  @click="open"><i class="fas fa-user-tag"></i>Tag friends</label>
+<label  @click="open"><i class="fas fa-laugh"></i>Feeling/Activity</label>
 </div>
 </div>
 
@@ -50,7 +52,43 @@
    <div class="elipsis"><i class="fas fa-ellipsis-h"></i></div>
     </div>
     <div class="description">
-        Hong Kong's neon signs are disappearing, and street photographer Sean Foley has made it his mission to capture them before it's too late. "Fading Memories" is now live, exclusively on 
+        Hong Kong's neon signs are disappearing, and street photographer Sean Foley has made it his mission to capture them before it's too late."Fading Memories" is now live, exclusively on 
+    </div>
+    <div class="image">
+        <img src="https://scontent.fnbo8-1.fna.fbcdn.net/v/t45.1600-4/cp0/q75/spS444/p526x296/194484182_23848002198700391_4322638678952833879_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=68ce8d&_nc_ohc=OZxphRCudjUAX8UGyXf&_nc_ht=scontent.fnbo8-1.fna&oh=15ce24028cf1565c3efc01f0470ef16a&oe=6199D6F8">
+    </div>
+    <div class="more"></div>
+    <div class="engagement">
+        <div class="emoji">
+        <span><i class="fas fa-thumbs-up"></i></span>
+        <span><i class="fas fa-heart"></i></span>
+       <p>177k</p>
+        </div>
+        <div class="reviews">
+        <p>5.3k comments</p>
+        <p>2.6k Shares </p>
+        </div>
+    </div>
+    <div class="action">
+        <label><i class="fas fa-thumbs-up"></i>like</label>
+             <label><i class="far fa-comment-alt"></i>comment</label>
+                  <label><i class="fas fa-share"></i>share</label>
+    </div>
+</div>
+<!-- 
+
+ -->
+
+<div class="card">
+    <div class="profile">
+        <div>
+        <img src="https://scontent.fnbo8-1.fna.fbcdn.net/v/t1.6435-9/72952939_2497393310353084_2684978412589678592_n.png?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=J8WEpcIH96QAX8nGTK0&_nc_ht=scontent.fnbo8-1.fna&oh=08b336607eb86dc1c0e47bac51c298a4&oe=61B96121">
+        <a href="https://crypto.com/nft/register?utm_source=facebook&utm_medium=fb&utm_campaign=NEW-Facebook%3AWW-en%3Aall%3ANFT-Conversion_DailyDrop_Registration&utm_content=20210601%20-%20Sean%20Foley&fbclid=IwAR3RZxLX-DczbTZypgAnSffd-N2xKvEmP2ubm9QmzHSuuIIxenLE1oS8jtE">crypto.com</a>
+   </div> 
+   <div class="elipsis"><i class="fas fa-ellipsis-h"></i></div>
+    </div>
+    <div class="description">
+        Hong Kong's neon signs are disappearing, and street photographer Sean Foley has made it his mission to capture them before it's too late."Fading Memories" is now live, exclusively on 
     </div>
     <div class="image">
         <img src="https://scontent.fnbo8-1.fna.fbcdn.net/v/t45.1600-4/cp0/q75/spS444/p526x296/194484182_23848002198700391_4322638678952833879_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=68ce8d&_nc_ohc=OZxphRCudjUAX8UGyXf&_nc_ht=scontent.fnbo8-1.fna&oh=15ce24028cf1565c3efc01f0470ef16a&oe=6199D6F8">
@@ -75,18 +113,74 @@
 </div>
 
 
-
 </div>
 <div class="part-2"></div>
 </div>
       </div>
+<div id="modal" v-if="modal">
+    <div class="create">
+        <h2>Create post</h2>
+      <button @click="open">X</button> 
+    </div>
+      <div class="post">
+  <span><i class="fas fa-user"></i></span>
+  <p>Victor Monderu</p>
+ </div>
+ <div>
+ <textarea></textarea>
+  <i class="fal fa-smile"></i>
+  </div>
+  <div class="include">
+      <div class="outline">
+          <div class="btn">
+<button>X</button>
+</div>
+<i class="fas fa-plus-square"></i>
+<h3>Add photos/videos</h3>
+<h6>or drag and drop</h6>
+</div>
+
+
+
+
+<div class="mobile">
+    <span><i class="fas fa-mobile-alt"></i></span>
+    <p>Add photos from your mobile device.</p>
+    <button>Add</button>
+</div>
+  </div>
+  <div class="segment-2">
+   <div class="add-emoji">
+<p>Add to your post</p>
+<label><i class="far fa-images"></i></label>
+<label><i class="fas fa-user-tag"></i></label>
+<label><i class="fas fa-laugh"></i></label>
+<label><i class="fas fa-map-marker-alt"></i></label>
+<label><i class="fas fa-ellipsis-h"></i></label>
+  </div>
+  <div class="enter">
+      <button>Post</button>
+  </div>
+</div>
+</div>
+
+
     </div>
 </template>
 <script>
 export default {
-    setup() {
-        
+    data() {
+       return{
+      modal:false,
+       } 
     },
+    methods: {
+      open:function(){
+          this.modal=!this.modal
+      }
+    }  
+     
+
 }
 </script>
 <style scoped>
@@ -135,10 +229,20 @@ nav{
     display: grid;
     grid-template-columns: repeat(4, 1fr);
 }
+.navigate span{
+ display: flex;
+justify-content: center;   
+border-radius:10px ;
+cursor: pointer;
+padding: 10px ;
+}
 
+.navigate span:hover{
+background: #f0f2f5;
+
+}
 .navigate i{
-    font-size: 28px;
-    
+    font-size: 28px;  
 }
 .fa-youtube{
     color: #65676b;
@@ -160,7 +264,8 @@ nav{
     border: 1px solid #65676b;
     display: flex;
     justify-content: center;
-    align-items: center;   
+    align-items: center;  
+    padding: 0; 
 }
 .user i{
     color: #65676b;
@@ -178,6 +283,7 @@ nav{
     display: flex;
     justify-content: center;
     align-items: center;
+    cursor: pointer;
 }
 .fa-user{
     color: #fff;
@@ -213,7 +319,7 @@ nav{
     flex-direction: column;
     border-radius:10px ;
     margin-top: 80px;
-  
+    box-shadow: 1px 1px 1px #ceced1;
 }
 .profile img{
     width: 50px;
@@ -227,6 +333,18 @@ nav{
     justify-content: space-between;
     padding: 12px 16px 0;
     margin-bottom: 12px;
+}
+.elipsis{
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.elipsis:hover{
+   background: #f0f2f5; 
 }
 .elipsis i{
     color: #65676b;
@@ -247,20 +365,21 @@ nav{
     border-bottom: 1px solid #65676b;
 }
 .engagement{
-    padding: 10px 0;
-    margin: 0 16px;
-    display: flex;
-    justify-content: space-between;
-    color: #65676b;
+ padding: 10px 0;
+  margin: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  color: #65676b;
   padding: 12px 16px 0;
+  border-bottom: 1px solid #65676b;
 }
 .emoji span:first-child{
  background: #216FD8;
  color: #fff;
  border-radius:50%;
-  display: flex;
-    justify-content: center;
-    align-items: center;   
+ display: flex;
+ justify-content: center;
+ align-items: center;   
  width: 20px;
  height: 20px;
  font-size: 12px;
@@ -270,9 +389,9 @@ nav{
  background:crimson;
  color: #fff;
  border-radius:50%;
-  display: flex;
-    justify-content: center;
-    align-items: center;   
+ display: flex;
+ justify-content: center;
+ align-items: center;   
  width: 20px;
  height: 20px;
  font-size: 12px;
@@ -312,6 +431,7 @@ label:hover{
     border-radius: 10px;
     margin-top: 80px;
     padding: 12px 16px 10px;
+    box-shadow: 1px 1px 1px #ceced1, 1px 1px 1px #ceced1 ;
 }
 .post span{
     width: 40px;
@@ -323,10 +443,12 @@ label:hover{
     align-items: flex-end;
     font-size: 30px;
     border: 1px solid #d3d4d4;
+    cursor: pointer;
 }
 .post{
     display: flex;
     gap: 20px;
+    padding: 12px 16px 16px;
 }
 .post input{
 padding: 8px 12px;
@@ -335,7 +457,135 @@ color: #65676b;
 background: #f0f2f5;
 border-radius: 36px;
 border: none;
+width: 80%;
+cursor: pointer;
+}
+.post-2{
+    border-top: 1px solid #65676b;
+   display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 12px 16px 16px;   
+}
+.fa-images{
+    color: rgb(8, 250, 8);
+}
+.fa-user-tag{
+    color: #216FD8;
+}
+.fa-laugh{
+    color: orange;
+}
+textarea{
+    border: none;
+    
+}
+button{
+    cursor: pointer;
+}
+#modal{
+    position:fixed;
+    background: #fff;
+    top: 15%;
+    left: 70%;
+    border-radius: 10px;
+    padding: 16px;
+}
+.create{
+    display: flex;
+    justify-content: space-between;
+    height: 60px;
+    align-items: center;
+}
+.create button{
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border:none;
+}
 
+.add-emoji{
+    display: flex;
+    padding: 8px;
+    border: 0.5px solid #ceced1;
+    border-radius: 10px;
+    gap: 10px;
+}
+.enter button{
+   background: #216FD8; 
+   color: #fff;
+   border-radius: 10px;
+   width:calc(100% - 16px);
+   display: flex;
+   align-items: center;
+   padding: 0 12px;
+   justify-content: center;
+   border: none;
+   height: 36px;
+}
+.include{
+    border: 1px solid #65676b;
+    border-radius: 10px;
+    padding: 8px ;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+.outline{
+    background: #f0f2f5;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    height: 228px;
+    padding: 10px;
+}
+.fa-plus-square{
+    margin:  auto;
+}
+h3, h6{
+ margin:  auto;
+
+}
+h6{
+    color: #65676b;
+}
+.btn{
+    display: flex;
+    justify-content: flex-end;
+}
+.outline button{
+    width: 36px;
+    height: 36px;
+    background: #fff;
+    border:0.5px solid #ceced1;
+    border-radius: 50%; 
+}
+.mobile{
+   background: #f0f2f5; 
+   border-radius: 10px;
+   display: flex;
+   padding: 8px;
+   gap: 10px;
+   align-items: center;
+}
+.mobile button{
+    border: none;
+    padding: 0 12px;
+    background: #e4e6eb;
+    height: 36px;
+    border-radius:5px ;
+}
+.enter{
+    padding: 16px 16px 0;
+}
+.segment-2{
+    padding: 16px 0;
+}
+
+
+.part-1 input{
+    width: 100px;
+    height: 50px;
+    background: chartreuse;
 }
 @media all and (max-width: 850px){
     .part-1{

c5a9d451ca06e80a41ca0d87b6d423d3d8528b2a
TORMOND <55413781+TORMOND@users.noreply.github.com>
2022-05-30 14:59:26 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkWbp12XRoV38kEajV7hBahdxwUxSXiVc",
+  apiKey: "",
   authDomain: "meta-fb.firebaseapp.com",
   projectId: "meta-fb",
   storageBucket: "meta-fb.appspot.com",
